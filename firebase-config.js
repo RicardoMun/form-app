77d5aee36788ee9f84ede1e3a57950770a37a9f0
Ricardo <richy.2603@gmail.com>
2023-05-01 17:20:49 -0500

    Configuración inicial para firebase

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDXvBRS5ojgy3_suLcPpi_TddeHa63VA48",
+  authDomain: "forms-app-fca09.firebaseapp.com",
+  projectId: "forms-app-fca09",
+  storageBucket: "forms-app-fca09.appspot.com",
+  messagingSenderId: "936586047668",
+  appId: "1:936586047668:web:af6ea8daf8aadaa29227e1"
+};
